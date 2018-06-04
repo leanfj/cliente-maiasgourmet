c84b0ae2f95c1a369b7ce4377ff90007ad5571c0
leanfj <leanfj@hotmail.com>
2018-06-04 20:36:12 -0300

    Modificações de configuração de projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -13,4 +16,30 @@ gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.sass', ['sass']);
 });
 
-gulp.task('default', ['sass:watch']);
+//Concatenar Javascript
+const bootstrapJs = './node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+const jquery = './node_modules/jquery/dist/jquery.js';
+
+const mainJS = './js/main.js';
+
+gulp.task('concatjs', function() {
+  return gulp
+    .src([jquery, bootstrapJs, mainJS])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('./js/'))
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('concatjs:watch', function() {
+  gulp.watch('./js/*.js', ['concatjs']);
+});
+
+gulp.task('uglify', function() {
+  gulp
+    .src('./js/scripts.min.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('default', ['sass:watch', 'concatjs:watch']);
